034648a0fcb26c42f62dea4b99a837fbe4aaa1c8
apple <apple@appledeMacBook-Pro.local>
2017-08-18 14:34:42 +0800

    “修改配置”

--- README.md
+++ README.md
@@ -2,8 +2,16 @@
 	蓝风信子网络科技有限公司PC端前端框架
 	此版本可以用来使用，也可以用于学习
 	
-### 简介
-wasabi Design PC端框架是由蓝风信子前端团队基于react技术专为企业应用系统定制的技术平台。首先将前后端通信独立成一个类库，方便广大使用react技术的同学们无需引入jquery框架就可与后端通信。框架提供丰富的常规组件与功能组件，具体有良好的交互体验效果... 
+### 框架简介
+wasabi Design PC端框架是由蓝风信子前端团队基于react技术专为企业应用系统定制的技术平台。框架提供丰富的常规组件与功能组件，具体有良好的交互体验效果... 
+wasabi-api 将前后端通信独立成一个类库，方便广大使用react技术的同学们无需引入jquery框架就可与后端通信。
+ 
+
+### demo简介
+config文件夹为 webpack打包时的入口文件，分为单页面与多页面两种情况，具体请见 config/index.js 
+src 为开发文件夹
+template.html为自动生成html的模板
+
 
 ### 安装 依赖项
 npm install 原始安装所有依赖项
@@ -12,13 +20,13 @@ npm install babel-core --save
 cnpm install 淘宝镜像安装方式 
 
 ### 运行
-然后运行npm run build来编译
-运行npm run dev 来启动服务
+运行npm run dev 来启动服务,访问 https://localhost:8080。
+自动热更新，但如果是config/index的打包配置改变，请重新启动
 
-###开发说明
-开发一个新功能后需要打包的，只要参考entryconfig/index.js 中参数即可。
-多页面时比如CRM系统可将配置拆分为多个，最后在index.js中合并。
-单页面，则只有一个index配置
+### 生产
+然后运行npm run build来编译 生成dist文件夹，将其放到服务器上
+
+
 
 
 
--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Page, DataGrid,Input,Button, SearchBar,Form,Modal,Toolbar,SlidePanel} from "wasabiD";
+import { Page, DataGrid,Input,Button, SearchBar,Form,Modal,Toolbar,SlidePanel,CheckBox} from "wasabiD";
 import { ajax } from "wasabi-api";
 class ButtonDemo extends React.Component {
     constructor(props) {
@@ -60,16 +60,26 @@ class ButtonDemo extends React.Component {
       this.refs.slide.open();
       this.refs.modal.open();
     }
+    onChange()
+    {
+        this.setState({
+
+        })
+    }
     render() {
         return <Page>
             <SearchBar ref="form" >
-            <Input type="text" name="good1" label="good1" style={{width:500}}></Input>
-             <Input type="text" name="good2" label="good2"></Input>
-              <Input type="text" name="good3" label="good3"></Input>
+            <Input type="text" name="text" label="text" onChange={this.onChange.bind(this)} style={{width:500}}></Input>
+            <Input type="muti" name="muti" label="muti" title="请输入" placeholder="多行文本输出"></Input>
+              <Input type="date" name="date" label="date"></Input>
+              <Input type="datetime" name="datetime" label="datetime"></Input>
+              <Input type="daterange" name="daterange" label="daterange"></Input>
+              <Input type="datetimerange" name="datetimerange" label="datetimerange"></Input>
               <Input type="select" data={[{value:"1",text:"男"},{value:"2",text:"女"}]}></Input>
-              <Input type="picker" data={[{value:"1",text:"男"},{value:"2",text:"女"}]}></Input>
+              <Input type="picker" name="picker" label="picker" data={[{value:"1",text:"男"},{value:"2",text:"女"}]}></Input>
               <Input type="checkbox" data={[{value:"1",text:"男"},{value:"2",text:"女"}]}></Input>
               <Input type="radio" data={[{value:"1",text:"男"},{value:"2",text:"女"}]}></Input>
+              <CheckBox data={[{value:"1",text:"男"},{value:"2",text:"女"}]}></CheckBox>
               <Button name="btn" style={{width:100}} title="提交" onClick={this.onClick}> </Button>
           
             </SearchBar>
