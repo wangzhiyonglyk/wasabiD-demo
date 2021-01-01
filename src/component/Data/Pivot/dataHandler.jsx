4237aabe40958aa470ba820f3e2ec948b4405a9d
甜甜 <tiantian@tiantiandeMacBook-Air.local>
2021-01-02 04:39:24 +0800

    开发交叉表

--- src/component/Data/Pivot/dataHandler.jsx
+++ src/component/Data/Pivot/dataHandler.jsx
@@ -8,92 +8,102 @@ import Columns from "./Configuration/Columns";
 export default {
 
     /**
-     * 列维度的表头数据模型与行维度的树组件数据模型
-     * @param {*} columns 
+     * 生成所需要的数据模型
+     * @param {*} columns 列纬度
+     * @param {*} rows 行纬度
+     *  @param {*} values 统计列
+     * @param {*} data 数据
+     */
+    setData(columns = [], rows = [], values = [], data = []) {
+        this.setHeaders(columns, values, data);
+    },
+    /**
+     * 生成列纬度的表头
+     * @param {*} columns 列纬度
+     * @param {*} values 统计列
+     * @param {*} data 数据
+     */
+    setHeaders(columns = [], values = [], data = []) {
+        /**
+        * 有多少个列纬度，就应该表头有多少行，另外一个统计指标的行，就是表头全部的行
+         */
+        let headers = [];//列表头
+        for (let col = 0; col < columns.length + 1; col++) {
+            //根据列纬度的分组，添加一行
+            headers.push([])
+        };
+        let newData = [];//最终的表格正体中的数据
+        for (let i = 0; i < data.length; i++) {
+            let colnamestr = [];//用于表头列的关键字,包括前面列纬度的值总和，才是当前列的字段关键字
+            for (let col = 0; col < columns.length; col++) {
+                colnamestr.push(data[i][columns[col].name] || "");//保存前面加当前的，用于最终数据列取值的对应的字段name
+                //处理列
+                if (headers[col].filter(item => { return item.name == colnamestr.join("-") }) == 0) {
+                    //没有添加此列
+                    headers[col].push({
+                        label: data[i][columns[col].name] || "",//列显示的名
+                        name: colnamestr.join("-"),//列名，不会对应具体的数据列
+                    });//添加一个列
+                }
+            }
+            //处理数据行的列
+            for (let v = 0; v < values.length; v++) {
+                if (headers[columns.length].filter(item => { return item.name == (colnamestr.join("-") + "-" + values[v].name) }) == 0) {
+                    headers[columns.length].push({
+                        label: values[v].label + "(" + values[v].collectName + ")",//列显示的名
+                        name: colnamestr.join("-") + "-" + values[v].name,//会对应具体的数据列，
+                        colSpan: 1,//数据行的列，只占一列
+                    });//添加一个列
+                }
+
+            }
+        }
+        //排序 todo 此处要改进
+        for (let col = 0; col < columns.length + 1; col++) {
+            //冒泡
+            for (let i = 0; i < headers[col].length - 1; i++) {
+                for (let j = 0; j < headers[col].length - i - 1; j++) {
+                    if (headers[col][j].name > headers[col][j + 1].name) {
+                        let temp = headers[col][j];
+                        headers[col][j] = headers[col][j + 1];
+                        headers[col][j + 1] = temp;
+                    }
+                }
+            }
+        }
+        //计算所跨列数
+        for (let col = 0; col < columns.length; col++) {
+            for (let i = 0; i < headers[col].length; i++) {
+                let colSpan = 0;
+                for (let j = 0; j < headers[columns.length].length; j++) {//通过最后一行数据行来判断
+                    if (headers[columns.length][j].name.indexOf(headers[col][i].name) > -1) {
+                        colSpan++;
+                    }
+                }
+                headers[col][i].colSpan = colSpan;
+            }
+        }
+        console.log("header", headers);
+    },
+
+    /**
+     * 生行纬度的树
      * @param {*} rows 
      * @param {*} data 
      */
-    setRowsAndColumns(columns, rows, data) {
-      return;
-        if (data && data instanceof Array) {
-            for (let i = 0; i < data.length; i++) {
-                for (let c = 0; c < columns.length; c++) {
-                    if (!columns[c].data) { columns[c].data = [] };
-                    if (columns[c].rows.indexOf(data[i][columns[c].name]) <= -1) {
-                        columns[c].rows.push(data[i][columns[c].name]);//插入
-                    }
-
-                }
-
-                for (let r = 0; r < rows.length; r++) {
-                    if (!rows[r].data) { rows[r].data = [] };
-                    if (rows[r].data.indexOf(data[i][rows[r].name]) <= -1) {
-                        rows[r].data.push(data[i][rows[r].name]);//插入
-                    }
-
-                }
+    setRowsTree(rows = [], data = []) {
+        let rowsTreeData = [];
+        //生成简单的数据结构放入tree,就可以了
+        for (let i = 0; i < data.length; i++) {
+            let obj = {
             }
+            let rowidstr="";//用于树组件
+            for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+                //
+                if(rowsTreeData.filter(item=>item.id==)
 
-        }
-        console.log(columns)
-        this.setDataHeader(columns);
-    },
-    /**
-     * 将列数据转成多行表头
-     * @param {*} columns 
-     */
-    setDataHeader(columns) {
-        let headers = [];
-        for (let i = 0; i < columns.length; i++) {
-            if (i == 0) {
-                headers.push(this.pushHeaderTr([], columns[i]))
-            } else {
-                headers.push(this.pushHeaderTr(headers[i - 1], columns[i]))
+
             }
         }
-        //设置所占的列数
-
-        for(let i=0;i<headers.length-1;i++){
-            if(headers[i].length>0){
-                let colSpan=headers[headers.length-1].length/headers[i].length;
-               
-                for(let j=0;j<headers[i].length;j++){
-                    headers[i][j].colSpan=colSpan;
-                }
-            }
-          
-        }
-     console.log("eh",headers)
-        return headers;
-    },
-
-    /**
-     * 生成当前行的表头
-     * @param {*} preTr 上一行的表头
-     * @param {*} columnsData 下一行的行数据
-     */
-    pushHeaderTr( preTr, columnsData) {
-        let tr = []
-        if (preTr.length==0) {
-            for (let i = 0; i < columnsData.data.length; i++) {
-                tr.push({
-                    name:  columnsData.data[i],
-                    label: columnsData.data[i],
-                    colSpan: 1,//默认设置
-                })
-            }
-        }
-        else {
-            for (let i = 0; i < preTr.length; i++) {
-                for (let j = 0; j < columnsData.data.length; j++) {
-                    tr.push({
-                        name:  preTr[i].name+ "-" + columnsData.data[j],
-                        label: columnsData.data[j],
-                        colSpan: 1,//默认设置
-                    })
-                }
-            }
-        }
-        return tr;
     }
 }
--- src/component/Data/Pivot/index.jsx
+++ src/component/Data/Pivot/index.jsx
@@ -13,107 +13,246 @@ import Tree from "../Tree";
 import Configuration from "./Configuration";
 import mixins from "../../Mixins/mixins"
 import dataHandler from "./dataHandler"
-import ("./index.css")
+import("./index.css")
 class Pivot extends Component {
     constructor(props) {
         super(props);
-        let p={ 
-            fields:func.clone(this.props.fields),//所有的字段
-            rows:func.clone(this.props.rows),//行维度
-            columns:func.clone(this.props.columns),//列维度
-            values:func.clone(this.props.values),//统计参数
-            filters:func.clone(this.props.filters),//筛选条件
-            data:func.clone(this.props.data),//数据
-            headers:[[{label:"树结构",colSpan:2}],[{name:"id",label:"id",sortAble:true,},{name:"name",label:"名称"}]],
+        let p = {
+            fields: func.clone(this.props.fields),//所有的字段
+            rows: func.clone(this.props.rows),//行维度
+            columns: func.clone(this.props.columns),//列维度
+            values: func.clone(this.props.values),//统计参数
+            filters: func.clone(this.props.filters),//筛选条件
+            data: func.clone(this.props.data),//数据
+            headers: [{ label: "省", name: "province" }, { label: "市", name: "city" }, { label: "年", name: "year" }, { label: "性别", name: "sex" }],
         }
-        let s={};
-        for(let key in p){
-            s[key]=p[key];
-            s["old"+key]=p[key];//保留一份方便更新
+        let s = {};
+        for (let key in p) {
+            s[key] = p[key];
+            s["old" + key] = p[key];//保留一份方便更新
         }
-        this.state =s;
+        this.state = s;
     }
     static getDerivedStateFromProps(nextProps, prevState) {
-        let newState={};
-        for(let key in nextProps){
-            if(prevState.hasOwnProperty(["old"+key])&&diff(prevState["old"+key],nextProps[key])){
+        let newState = {};
+        for (let key in nextProps) {
+            if (prevState.hasOwnProperty(["old" + key]) && diff(prevState["old" + key], nextProps[key])) {
                 //更新此字段
-                newState["old"+key]=nextProps[key];
-                newState[key]=nextProps[key];
+                newState["old" + key] = nextProps[key];
+                newState[key] = nextProps[key];
             }
         }
-        if(func.isEmptyObject(newState)){
+        if (func.isEmptyObject(newState)) {
             return null;
         }
-        else{
+        else {
             return newState;
         }
-       
+
     }
-    componentDidMount(){
-        this.initData();
-        setTimeout(()=>{
+    componentDidMount() {
+     
+        setTimeout(() => {
             this.setState({
-                data:[ { id:1, pId:0, name:"父节点1 - 展开", open:true},
-                { id:11, pId:1, name:"父节点11 - 折叠"},
-                { id:111, pId:11, name:"叶子节点111"},
-                { id:112, pId:11, name:"叶子节点112"},
-                { id:113, pId:11, name:"叶子节点113"},
-                { id:114, pId:11, name:"叶子节点114"},
-                { id:12, pId:1, name:"父节点12 - 折叠"},
-                { id:121, pId:12, name:"叶子节点121"},
-                { id:122, pId:12, name:"叶子节点122"},
-                { id:123, pId:12, name:"叶子节点123"},
-                { id:124, pId:12, name:"叶子节点124"},
-                { id:13, pId:1, name:"父节点13 - 没有子节点", isParent:true},
-                { id:2, pId:0, name:"父节点2 - 折叠"},
-                { id:21, pId:2, name:"父节点21 - 展开", open:true},
-                { id:211, pId:21, name:"叶子节点211"},
-                { id:212, pId:21, name:"叶子节点212"},
-                { id:213, pId:21, name:"叶子节点213"},
-                { id:214, pId:21, name:"叶子节点214"},
-                { id:22, pId:2, name:"父节点22 - 折叠"},
-                { id:221, pId:22, name:"叶子节点221"},
-                { id:222, pId:22, name:"叶子节点222"},
-                { id:223, pId:22, name:"叶子节点223"},
-                { id:224, pId:22, name:"叶子节点224"},
-                { id:23, pId:2, name:"父节点23 - 折叠"},
-                { id:231, pId:23, name:"叶子节点231"},
-                { id:232, pId:23, name:"叶子节点232"},
-                { id:233, pId:23, name:"叶子节点233"},
-                { id:234, pId:23, name:"叶子节点234"},
-                { id:3, pId:0, name:"父节点3 - 没有子节点", isParent:true}]
-              })
-        },1500)
-       
-        
+                rows: [
+                    {
+                        name: "province",
+                        label: "省",
+                    },
+                    {
+                        name: "city",
+                        label: "市"
+                    }
+                ],
+                columns: [
+                    {
+                        name: "year",
+                        label: "年"
+                    }, {
+                        name: "sex",
+                        label: "性别"
+                    }
+                ],
+                values:[
+                    {
+                        name: "income",
+                        label: "收入",
+                        collectName:"平均值",
+                        collectType:"avg"
+
+                    }, {
+                        name: "consume",
+                        label: "消费",
+                        collectName:"平均值",
+                        collectType:"avg"
+
+                    }
+                ],
+                data: [{
+                    province: "湖南省",
+                    city: "长沙市",
+                    year: "2019",
+                    sex: "男",
+                    income: 10000,
+                    consume: 8000
+                },
+                {
+                    province: "湖南省",
+                    city: "长沙市",
+                    year: "2020",
+                    sex: "男",
+                    income: 11000,
+                    consume: 10000
+                },
+                {
+                    province: "湖南省",
+                    city: "长沙市",
+                    year: "2019",
+                    sex: "女",
+                    income: 11000,
+                    consume: 8000
+                },
+                {
+                    province: "湖南省",
+                    city: "长沙市",
+                    year: "2020",
+                    sex: "女",
+                    income: 12000,
+                    consume: 12000
+                },
+
+                {
+                    province: "湖南省",
+                    city: "衡阳市",
+                    year: "2019",
+                    sex: "男",
+                    income: 13000,
+                    consume: 8500
+                },
+                {
+                    province: "湖南省",
+                    city: "衡阳市",
+                    year: "2020",
+                    sex: "男",
+                    income: 12000,
+                    consume: 11000
+                },
+                {
+                    province: "湖南省",
+                    city: "衡阳市",
+                    year: "2019",
+                    sex: "女",
+                    income: 13000,
+                    consume: 8300
+                },
+                {
+                    province: "湖南省",
+                    city: "衡阳市",
+                    year: "2020",
+                    sex: "女",
+                    income: 14000,
+                    consume: 14000
+                },
+
+                {
+                    province: "广东省",
+                    city: "广州市",
+                    year: "2019",
+                    sex: "男",
+                    income: 11000,
+                    consume: 8400
+                },
+                {
+                    province: "广东省",
+                    city: "广州市",
+                    year: "2020",
+                    sex: "男",
+                    income: 14000,
+                    consume: 12000
+                },
+                {
+                    province: "广东省",
+                    city: "广州市",
+                    year: "2019",
+                    sex: "女",
+                    income: 15000,
+                    consume: 12900
+                },
+                {
+                    province: "广东省",
+                    city: "广州市",
+                    year: "2020",
+                    sex: "女",
+                    income: 13000,
+                    consume: 12000
+                },
+
+                {
+                    province: "广东省",
+                    city: "深圳市",
+                    year: "2019",
+                    sex: "男",
+                    income: 14000,
+                    consume: 8800
+                },
+                {
+                    province: "广东省",
+                    city: "深圳市",
+                    year: "2020",
+                    sex: "男",
+                    income: 15000,
+                    consume: 11400
+                },
+                {
+                    province: "广东省",
+                    city: "深圳市",
+                    year: "2019",
+                    sex: "女",
+                    income: 14000,
+                    consume: 8600
+                },
+                {
+                    province: "广东省",
+                    city: "深圳市",
+                    year: "2020",
+                    sex: "女",
+                    income: 15000,
+                    consume: 14700
+                },
+                ]
+
+            },()=>{this.initData()})
+        }, 1500)
+
+
     }
-    initData(){
-        this.setRowsAndColumns(this.state.columns,this.state.rows,this.state.data);
+    
+    initData() {
+        this.setData(this.state.columns, this.state.rows,this.state.values, this.state.data);
     }
-    render(){
+    render() {
         console.log(this.state.data)
-        let treeTop=0;
-        if(this.state.headers instanceof Array&&this.state.headers.length>0){
-            if(this.state.headers[0] instanceof Array){
-                treeTop=this.state.headers.length*41;
+        let treeTop = 0;
+        if (this.state.headers instanceof Array && this.state.headers.length > 0) {
+            if (this.state.headers[0] instanceof Array) {
+                treeTop = this.state.headers.length * 41;
             }
-            else{
-                treeTop=41;
+            else {
+                treeTop = 41;
             }
         }
-       return <div className="wasabi-pivot">
-               <div className="wasabi-pivot-left">
-                  <Configuration height={treeTop}></Configuration>
-                  <div className="wasabi-pivot-rowsData" >
-                  <Tree  data={this.state.data} simpleData={true} idField="id" textField="name"></Tree>
-                  </div>
-               </div>
-               <div className="wasabi-pivot-right">
-               <DataGrid pagination={this.props.pagination}  rowNumber={false}
-    headers={this.state.headers} data={this.state.data}></DataGrid>
-               </div>
-       </div>
+        return <div className="wasabi-pivot">
+            <div className="wasabi-pivot-left">
+                <Configuration height={treeTop}></Configuration>
+                <div className="wasabi-pivot-rowsData" >
+                    <Tree data={this.state.data} simpleData={true} idField="city" parentField="province" textField="city"></Tree>
+                </div>
+            </div>
+            <div className="wasabi-pivot-right">
+                <DataGrid pagination={this.props.pagination} rowNumber={false}
+                    headers={this.state.headers} data={this.state.data}></DataGrid>
+            </div>
+        </div>
     }
 }
 
@@ -125,17 +264,17 @@ Pivot.propTypes = {
     values: PropTypes.array,//统计参数
     filters: PropTypes.array,//筛选条件
     data: PropTypes.array,//数据,
-    applyHandler:PropTypes.func,//请求数据处理
+    applyHandler: PropTypes.func,//请求数据处理
 }
 
 Pivot.defaultProps = {
-    fields:[],//所有的字段
+    fields: [],//所有的字段
     rows: [],//行维度
-    columns:[],//列维度
+    columns: [],//列维度
     values: [],//统计参数
-    filters:[],//筛选条件
-    data:[],//数据,
-    applyHandler:null,//请求数据处理
+    filters: [],//筛选条件
+    data: [],//数据,
+    applyHandler: null,//请求数据处理
 }
-mixins(Pivot,[dataHandler])
+mixins(Pivot, [dataHandler])
 export default Pivot;
